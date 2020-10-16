91af442274f3fdf2cd7c7bd7c5660736a197ad02
ppopow <pawel.popow@gmail.com>
2020-10-16 22:32:07 +0200

    nowe przyklady

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let name = 'Hans';
+
+name = 'Monika';
+
+let number = 8;
+
+let boolean = false;
+
+let array = [1, 2, 3, 108];
+
+let objectH1 = document.querySelector('h1');
+
+objectH1.innerText = 'Kurs Javascript';
+
+// if (number === 108) {
+//     console.log("Spełniony warunek number === 108");
+// }
+// else if (number === 110) {
+//     console.log("Spełniony warunek number === 110");
+// }
+// else {
+//     console.log("Żaden z powyżyszych warunków nie został spełniony");
+// }
+
+let result = number * 2;
+
+let numberTxt = '9';
+
+if (number !== numberTxt) {
+    console.log("Wartości zmiennych i ich typy są NIE równe");
+}
+
+
